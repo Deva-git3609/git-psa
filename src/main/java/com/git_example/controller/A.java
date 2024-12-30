a281dd9cfec281e895c28b86c40aaddf19f38fac
Deva <appjava46@gmail.com>
2024-12-30 12:50:46 +0530

    defect fixed - 3457 - Fixed null value not  to stored in database.

---
+++ src/main/java/com/git_example/controller/A.java
@@ -0,0 +1,7 @@
+package com.git_example.controller;
+
+public class A {
+    public void test() {
+        System.out.println("From test");
+    }
+}
--- src/main/java/com/git_example/controller/GitController.java
+++ src/main/java/com/git_example/controller/GitController.java
@@ -1,4 +1,7 @@
 package com.git_example.controller;
 
 public class GitController {
+    public static void main(String[] args) {
+        System.out.println("Hello World!.");
+    }
 }
